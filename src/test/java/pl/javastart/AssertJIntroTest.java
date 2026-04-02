8db9ffd293fde2924e7adcb8480125edf467f279
mabi0110 <mariadb0110gmail.com>
2026-04-02 16:15:12 +0200

    add multiple assertions with JUnit and AssertJ

--- src/test/java/pl/javastart/AssertJIntroTest.java
+++ src/test/java/pl/javastart/AssertJIntroTest.java
@@ -1,6 +1,9 @@
 package pl.javastart;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 
 public class AssertJIntroTest {
@@ -32,6 +35,32 @@ public class AssertJIntroTest {
                 .isEqualTo(name2)
                 .hasSize(6)
                 .endsWith("a");
+    }
+
+    @Test
+    public void multipleAssertionsWithJUnit() {
+
+        String name1 = "Dorota";
+        String name2 = "Dorota";
+
+        assertAll(
+                () -> assertThat(name1).isEqualTo(name2),
+                () -> assertThat(name1).hasSize(6),
+                () -> assertThat(name1).endsWith("a")
+        );
+    }
+
+    @Test
+    public void multipleAssertionsWithAssertJ() {
+
+        String name1 = "Dorota";
+        String name2 = "Dorota";
+
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(name1).isEqualTo(name2);
+        softAssertions.assertThat(name1).hasSize(6);
+        softAssertions.assertThat(name1).endsWith("a");
+        softAssertions.assertAll();;
     }
 
 }
